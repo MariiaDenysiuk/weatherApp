0d09918a074e2596579a8b922f867921fc4e892b
maryden <marydenysiuk@gmail.com>
2020-07-28 08:52:56 +0300

    added sun data

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@
     let max = document.getElementById("max");
     let min = document.getElementById("min");
     let currentTemp = 0;
-    var weather = {'Clouds': "./video/cloud.mp4", "Rain": "./video/rain.mp4", "Sun": "./video/sun.mp4"}
+    var weather = {'Clouds': "./video/cloud.mp4", "Rain": "./video/rain.mp4", "Clear": "./video/sun.mp4"}
 
     function getLocation() {
         if (navigator.geolocation) {
@@ -18,26 +18,27 @@
     }
 
     function showPosition(position) {
-    const link = 'https://fcc-weather-api.glitch.me/' + '/api/current?lat='+position.coords.latitude.toFixed()+'&lon='+position.coords.longitude.toFixed();
-    
-    fetch(link).then((response) => response.json()).then(
-        (response) => {
-            const t = response.main.temp;  
-            temp.innerHTML = t;
-            currentTemp = t;
+        const link = 'https://fcc-weather-api.glitch.me/' + '/api/current?lat='+position.coords.latitude.toFixed()+'&lon='+position.coords.longitude.toFixed();
+        
+        fetch(link).then((response) => response.json()).then(
+            (response) => {
+                const t = response.main.temp;  
+                temp.innerHTML = t;
+                currentTemp = t;
+            
+                humidity.innerHTML = response.main.humidity
+                max.innerHTML = response.main.temp_max;
+                min.innerHTML = response.main.temp_min;
 
-            humidity.innerHTML = response.main.humidity
-            max.innerHTML = response.main.temp_max;
-            min.innerHTML = response.main.temp_min;
-
-            video.src = weather[response.weather[0].main];
-        }
-    )
+                video.src = weather[response.weather[0].main];
+            }
+        )
     }
 
     button[0].addEventListener("click", function(event) {
         temp.innerHTML = currentTemp;  
     });
+
     button[1].addEventListener("click", function(event) {
         const farang = farangeit(currentTemp);
         temp.innerHTML = farang;
